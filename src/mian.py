a910c9dcb6f13c26e72ae510f3508a5056971200
musuobunengji <2480527390@qq.com>
2025-12-14 16:24:02 +0800

    -Introduce and Abstract validation for node and edge

--- src/mian.py
+++ src/mian.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 from json_helper_load import load_nodes, load_edges
+from data_load_to_graph import validate_node, validate_edge
 
 G = nx.Graph()
 
@@ -10,13 +11,15 @@ edges = load_edges()
 
 # 节点
 for node in nodes:
-    progress_percent = node["content"]["progress_percentage"]
-    G.add_node(node["id"], progress_percent=progress_percent)
+    if validate_node(node_data=node):
+        progress_percent = node["content"]["progress_percentage"]
+        G.add_node(node["id"], progress_percent=progress_percent)
 
 # 关系
-for edge in edges:
-    G.add_edge(edge["from"], edge["to"])
 
+for edge in edges:
+    if validate_edge(edge_data=edge, nodes=nodes):
+        G.add_edge(edge["from"], edge["to"])
 # 画图
 pos = nx.spring_layout(G)
 nx.draw(G, pos, with_labels=True, node_size=1200)
